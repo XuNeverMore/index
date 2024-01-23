248ccca83b03aafdd580fea7c8608dfacbb2bba9
chuanting.xu <chuanting.xu@ucoofun.com>
2024-01-23 17:01:14 +0800

    adjust small screen

---
+++ config.js
@@ -0,0 +1,39 @@
+const configs = {
+  websites: [
+    {
+      icon: "https://www.gstatic.cn/devrel-devsite/prod/v80bae38ba58d74b96b4842131d88ee335fbea404678aa063008110db834e2268/android/images/favicon.png",
+      net: "Android",
+      link: "https://developer.android.google.cn/",
+    },
+    {
+      icon: "https://developer.mozilla.org/favicon-48x48.cbbd161b.png",
+      net: "MDN",
+      link: "https://developer.mozilla.org/zh-CN/",
+    },
+    {
+      icon: "https://www.cnblogs.com/images/aggsite/myblog.svg",
+      net: "Blog",
+      link: "https://www.cnblogs.com/",
+    },
+    {
+      icon: "https://www.bilibili.com/favicon.ico?v=1",
+      net: "Bilibili",
+      link: "https://www.bilibili.com/",
+    },
+    {
+      icon: "https://github.githubassets.com/favicons/favicon.svg",
+      net: "Github",
+      link: "https://github.com/",
+    },
+    {
+      icon: "https://gitee.com/favicon.ico",
+      net: "Gitee",
+      link: "https://gitee.com/",
+    },
+    {
+      icon: "icons/ic_iqy.png",
+      net: "iQiYi",
+      link: "https://www.iqiyi.com/",
+    },
+  ],
+};
---
+++ scripts/loader.js
@@ -0,0 +1,90 @@
+let netConfig = configs.websites;
+window.addEventListener("load", (event) => {
+  const content = document.querySelector("#content");
+  netConfig.forEach(function (value) {
+    // console.log(value.net)
+    // console.log(value.link)
+    let nodeItem = document.createElement("li");
+    nodeItem.className = "item";
+    nodeItem.innerHTML = `<a href=${value.link} target=${value.link}>
+                        <div>
+                            <div class='img_container'>
+                                <img src=${value.icon}>
+                            </div>
+                            <span class='item_span'>${value.net}</span>
+                        </div>
+                    </a>`;
+    content.append(nodeItem);
+  });
+  //body click
+  document.querySelector(".main").addEventListener("click", () => {
+    drawerManager.closeDrawer();
+  });
+  engines.setBingEngine();
+  //init bg color
+  let bgColorStart = docCookies.getItem("bgColorStart");
+  let bgColorEnd = docCookies.getItem("bgColorEnd");
+  console.log("start:" + bgColorStart + ",end:" + bgColorEnd);
+  document.querySelector("#bgcolor-start").value =
+    bgColorStart == null ? "#1E4B73" : bgColorStart;
+  document.querySelector("#bgcolor-end").value =
+    bgColorEnd == null ? "#FFFFFF" : bgColorEnd;
+  themeManager.applyLinearBgColor();
+});
+
+function getCurrentTime() {
+  var date = new Date();
+  date.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+  // return date.format("yyyyMMdd")
+  var year = date.getFullYear();
+  var month = date.getMonth();
+  var day = date.getDay();
+  var hour = date.getHours();
+  var minute = date.getMinutes();
+  var sec = date.getSeconds();
+  // +hour+":"+minute+":"+sec
+  return "" + year + "年" + month + "月" + day + "日 ";
+}
+var onHeaderClicked = function () {
+  let state = drawerManager.toggle();
+  event.stopPropagation();
+  console.log("=========clicked mainLogo");
+};
+drawerManager.onDrawerClosed = () => {
+  console.log("onDrawerClosed");
+  themeManager.applyLinearBgColor();
+};
+var onClickEmpty = () => {
+  event.stopPropagation();
+};
+var keys = docCookies.keys();
+for (var n = 0; n < keys.length; n++) {
+  console.log(docCookies.getItem(keys[n]));
+}
+var hasConfig = docCookies.hasItem("net_config");
+if (!hasConfig) {
+  // docCookies.setItem()
+  console.log("no config!!!");
+  docCookies.setItem("net_config", "xuchuanting_config");
+}
+function onBgColorStartChange(color) {
+  console.log("on color start change:" + color);
+  docCookies.setItem("bgColorStart", color, Infinity);
+}
+function onBgColorEndChange(color) {
+  console.log("on color end change:" + color);
+  docCookies.setItem("bgColorEnd", color, Infinity);
+}
+function search() {
+  let edt = document.querySelector("#edt-search");
+  let content = edt.content;
+  console.log("========edt content:" + content);
+}
+function onEngineChanged(engineName) {
+  console.log("engine===" + engineName);
+  engines.setEngine(engineName);
+}
+function showSelect() {
+  var sel = document.querySelector("#select-engine");
+  sel.onclick();
+}
